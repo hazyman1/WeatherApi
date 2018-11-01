436d2f7d80a02dabc9da2794206b327f53bedb39
Hays <hayblackwell@gmail.com>
2018-11-01 08:17:17 -0500

    final commit

--- ConsoleApp21/Program.cs
+++ ConsoleApp21/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.net
 
 namespace ConsoleApp21
 {
@@ -6,7 +7,23 @@ namespace ConsoleApp21
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What Zip?");
+            int answer = int.Parse(Console.ReadLine();
+            Console.Clear();
+
+            WebClient webClient = new WebClient();
+            string apiKey = "" //FileStyleUriParser.ReadAllText("Path to file");
+            string fullApi = $""
+
+            string apiResponse = webClient.DownloadString(fullApi);
+            JObject jo = JObject.Parse(apiResponse);
+            double temp = double.Parse(jo.GetValue("main")["temp"].ToString());
+
+            double fTemp = temp * 9 / 5 - 459.67;
+            fTemp = Math.Round(fTemp, 1);
+
+            Console.WriteLine($"The Temperature is {fTemp} f.");
+            Console.ReadLine();
         }
     }
 }
